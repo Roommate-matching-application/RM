f772ce5a4cb2e5f2d0280caabfc54089358bc2d2
KimGaEun <dkrk333@naver.com>
2022-03-24 11:01:59 +0900

    commit

---
+++ app/src/main/java/com/example/mop125/afterlogin.java
@@ -0,0 +1,33 @@
+package com.example.mop125;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class afterlogin extends AppCompatActivity {
+    private Button GoTest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afterlogin);
+        GoTest = (Button) findViewById(R.id.nextButton);
+//여기로 안넘어옴!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+    }
+}
