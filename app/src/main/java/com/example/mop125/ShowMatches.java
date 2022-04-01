2cfbb4bef9306865e94955293205d03194a546f6
Dami Kim <sspdami00@gmail.com>
2022-04-01 22:33:06 +0900

    매치 보여주기

---
+++ app/src/main/java/com/example/mop125/ShowMatches.java
@@ -0,0 +1,39 @@
+package com.example.mop125;
+
+
+    import androidx.appcompat.app.AppCompatActivity;
+
+    import android.content.Context;
+    import android.os.Bundle;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.LinearLayout;
+
+
+public class ShowMatches extends AppCompatActivity {
+
+    LinearLayout container;
+    LayoutInflater inflater;
+    Button getInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_matches);
+
+
+        container = (LinearLayout) findViewById(R.id.container);
+        inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        getInfo.setOnClickListener(new getInfoClickListener());}
+
+    protected class getInfoClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            container.removeAllViews();
+            inflater.inflate(R.layout.activity_show_data, container, true);
+        }
+    }
+}
