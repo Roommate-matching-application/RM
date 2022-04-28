1efcdb7b992b31a355bbba1cc1fcc9881a961377
sangmin <sm524233@naver.com>
2022-04-29 02:21:42 +0900

    체크리스트값 firebase 저장

--- app/src/main/java/com/example/mop125/myChecklist.java
+++ app/src/main/java/com/example/mop125/myChecklist.java
@@ -3,21 +3,34 @@ package com.example.mop125;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class myChecklist extends AppCompatActivity {
 
     static SharedPreferences sharedPreferences;
     static RadioGroup rg1_sleeptime;
     static RadioGroup rg2_wakeuptime;
+    Button btn_my;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    Member member;
+    int i = 0;
 
-
-    String[] items = {"95", "96", "97", "98", "99", "00", "01", "02","03","04"};
+    String[] items = {"95", "96", "97", "98", "99", "00", "01", "02","03","04"}; //나이
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +39,46 @@ public class myChecklist extends AppCompatActivity {
         initPreferences();
         loadState();
 
+        reference = database.getInstance().getReference().child("age");
+        member = new Member();
+
+        btn_my = findViewById(R.id.btn_my);
+        rg1_sleeptime = findViewById(R.id.rg1_sleeptime);
+        rg2_wakeuptime = findViewById(R.id.rg2_wakeuptime);
+
+        //firebase
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    i = (int)snapshot.getChildrenCount();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //저장버튼 누르면 firebase에 저장
+        btn_my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                member.setRg1(String.valueOf((int)rg1_sleeptime.getCheckedRadioButtonId()));
+                reference.child(String.valueOf(i+1)).setValue(member);
+
+                member.setRg2(String.valueOf((int)rg2_wakeuptime.getCheckedRadioButtonId()));
+                reference.child(String.valueOf(i+1)).setValue(member);
+            }
+        });
+
+        //나이 선택 spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_dropdown_item, items
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
         spinner.setSelection(sharedPreferences.getInt("spinner_age", 1));
     }
 
--- app/src/main/java/com/example/mop125/roommateChecklist.java
+++ app/src/main/java/com/example/mop125/roommateChecklist.java
@@ -1,20 +1,34 @@
 package com.example.mop125;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class roommateChecklist extends AppCompatActivity {
 
     static SharedPreferences sharedPreferences;
     private CheckBox cg1_1,cg1_2,cg1_3,cg1_4,cg1_5,cg1_6,cg1_7,cg1_8,cg2_1,cg2_2;
+    Button btn_rm;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    Member member;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,83 @@ public class roommateChecklist extends AppCompatActivity {
         setContentView(R.layout.activity_roommate_checklist);
         initPreferences();
         loadState();
+
+        reference = database.getInstance().getReference().child("age");
+        member = new Member();
+
+        btn_rm = findViewById(R.id.btn_rm);
+        cg1_1 = findViewById(R.id.cg1_1);
+        cg1_2 = findViewById(R.id.cg1_2);
+        cg1_3 = findViewById(R.id.cg1_3);
+        cg1_4 = findViewById(R.id.cg1_4);
+        cg1_5 = findViewById(R.id.cg1_5);
+        cg1_6 = findViewById(R.id.cg1_6);
+        cg1_7 = findViewById(R.id.cg1_7);
+        cg1_8 = findViewById(R.id.cg1_8);
+        cg2_1 = findViewById(R.id.cg2_1);
+        cg2_2 = findViewById(R.id.cg2_2);
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    i = (int)snapshot.getChildrenCount();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        btn_rm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cg1_1.isChecked()){
+                    member.setAge1(cg1_1.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg1_2.isChecked()){
+                    member.setAge2(cg1_2.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg1_3.isChecked()){
+                    member.setAge3(cg1_3.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg1_4.isChecked()){
+                    member.setAge4(cg1_4.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg1_5.isChecked()){
+                    member.setAge5(cg1_5.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg1_6.isChecked()){
+                    member.setAge6(cg1_6.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg1_7.isChecked()){
+                    member.setAge7(cg1_7.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg1_8.isChecked()){
+                    member.setAge8(cg1_8.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg2_1.isChecked()){
+                    member.setSmoke1(cg2_1.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+                if(cg2_2.isChecked()){
+                    member.setSmoke2(cg2_2.getText().toString());
+                    reference.child(String.valueOf(i+1)).setValue(member);
+                }
+            }
+        });
+
     }
 
     @Override
